75e921cbed3aecc1e2af13402946587bb1ff3555
seyi-adeleke <seyiadeleke42@yahoo.com>
2016-06-09 16:29:40 +0100

    code refactor

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
  * Created by seyi adeleke on 5/28/2016.
  */
 $(document).ready(function(){
-    $('#search-button').click(function() {
+
+    var SearchCallBack = function(){
         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + $('#searcher').val();
         console.log(url);
         $.ajax({
@@ -12,15 +13,20 @@ $(document).ready(function(){
             success:function(data){
                 $("#result").empty();
                 for(i=0;i<data[1].length;i++){
-                   $("#result").append("<div class=\"well container-fluid\"><p><a id=\"links\" href="
-                       + data[3][i] + "><b>" + data[1][i] + "</b></a></p><p>" + data[2][i] + "</p></div>");
+                    $("#result").append("<div class=\"well container-fluid\"><p><a id=\"links\" href="
+                        + data[3][i] + "><b>" + data[1][i] + "</b></a></p><p>" + data[2][i] + "</p></div>");
                 }
             },
             error:function(){
                 console.log("error");
             }
 
-           });
-
         });
+    };
+    $("#searchbox").keypress(function() {
+        if (event.which == 13) {
+            SearchCallBack();
+        }
+    });
+    $('#search-button').click(SearchCallBack);
 });
